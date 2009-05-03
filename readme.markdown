b81efc345734dfdf03149b75400c6f3f095b4441
Dirk Ginader <dirk@ginader.de>
2009-05-03 15:37:25 +0100

    initial commit of version 1.0 that previoulsy was in svn

---
+++ readme.markdown
@@ -0,0 +1,19 @@
+jQuery Accessible Tabs Plugin
+=============================
+
+jQuery Plugin that nicely outlines the targets of the anchors in the given selector.
+By doing so it mimics the behavior seen at http://stockholm.se/ but instead of the CSS 2 Property "outline"
+like stockholm.se does this script draws an empty div to achieve the same result but also
+for older Browsers like Internet Explorers.
+
+Usage
+-----
+(function($) {
+    $('#skip a').skipnavHighlight();
+})(jQuery);
+
+See also
+--------
+* english info: http://blog.ginader.de/archives/2008/12/11/jQuery-skipnavHighlight-nicely-highlighting-skip-link-targets.php
+* german info: http://blog.ginader.de/archives/2008/12/11/jQuery-skipnavHighlight-Skip-Link-Ziele-Anschaulich-machen.php
+
---
+++ skipnavhighlight.jquery.js
@@ -0,0 +1,57 @@
+/**
+ * skip link highlight - jQuery plugin for accessible, unobtrusive highglighting of skip link targets
+ * Inspired by http://stockholm.se/
+ * @requires jQuery v1.2.6
+ * Usage:
+ * $("a").skipnavHighlight();
+ *
+ * http://blog.ginader.de
+ *
+ * Copyright (c) 2007 Dirk Ginader (ginader.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Version: 1.0
+ */
+(function($) {
+    $.fn.extend({
+        showSkipTargetHighlight : function(o){
+            var el = $( $(o).attr('href') );
+            var pos = el.offset();
+            this.targetOverlay.css({top:pos.top,left:pos.left,width:el.width(),height:el.height()}).show();
+        },
+        skipnavHighlight: function(conf) {
+            var defaults = {
+                targetClass: 'skiptarget' // Classname to apply to the element the anchor points to
+            };
+            var options = $.extend(defaults, conf);
+            var o = this;
+            if(!this.targetOverlay){
+                this.targetOverlay = document.createElement('div');
+                this.targetOverlay.className = options.targetClass;
+                $('body').append(this.targetOverlay);
+                this.targetOverlay = $(this.targetOverlay).hide();
+                this.targetOverlay.mousedown(function(){
+                    o.targetOverlay.hide();
+                });
+            }
+            return this.each(function() {
+                $(this).hover(
+                    function() {
+                        o.showSkipTargetHighlight($(this));
+                    }, 
+                    function() {
+                        o.targetOverlay.hide();
+                    }
+                );
+                $(this).focus(function () {
+                    o.showSkipTargetHighlight($(this));
+                });
+                $(this).blur(function () {
+                    o.targetOverlay.hide();
+                });                
+            });
+        }
+    });
+})(jQuery);
